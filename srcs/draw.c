87315c6792084ba3cf80f1cb1be5d6aa7b4c321d
lohhiiccc <loic.rio25130@gmail.com>
2023-12-08 19:59:09 +0100

    minimize function parameter

--- srcs/draw.c
+++ srcs/draw.c
@@ -13,7 +13,8 @@
 #include "fractol.h"
 #include <math.h>
 
-void	draw_fractal(t_data *img, t_com_coord comp, int max_iterations,int (*f)(t_complex, int))
+#include <stdio.h>
+void draw_fractal(t_vars *vars, int max_iterations)
 {
 	t_pixel		pixel;
 	t_complex	c;
@@ -24,14 +25,14 @@ void	draw_fractal(t_data *img, t_com_coord comp, int max_iterations,int (*f)(t_c
 		pixel.y = 0;
 		while (pixel.y < W_H)
 		{
-			c.real = comp.min.real + (comp.max.real - comp.min.real) * pixel.x / W_W;
-			c.imag = comp.min.imag + (comp.max.imag - comp.min.imag) * pixel.y / W_H;
-			pixel.iterations = f(c, max_iterations);
+			c.real = vars->info.comp.min.real + (vars->info.comp.max.real - vars->info.comp.min.real) * pixel.x / W_W;
+			c.imag = vars->info.comp.min.imag + (vars->info.comp.max.imag - vars->info.comp.min.imag) * pixel.y / W_H;
+			pixel.iterations = vars->info.fractal_func(c, max_iterations);
 			pixel.r = (int)(sin(0.05 * pixel.iterations) * 127);
 			pixel.g = (int)(sin(0.03 * pixel.iterations) * 225);
 			pixel.b = (int)(sin(0.01 * pixel.iterations) * 127);
 			pixel.rgb = (pixel.r << 16) | (pixel.g << 8) | pixel.b;
-			my_mlx_pixel_put(img, pixel.x, pixel.y, pixel.rgb);
+			my_mlx_pixel_put(&vars->img, pixel.x, pixel.y, pixel.rgb);
 			pixel.y++;
 		}
 		pixel.x++;
